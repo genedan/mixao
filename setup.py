28503eb195ce5129879679226fe1b68b5d74f7f2
Gene Dan <genedan@gmail.com>
2023-08-24 23:40:26 -0500

    FEAT: Initialize package setup.

---
+++ setup.py
@@ -0,0 +1,44 @@
+import re
+import setuptools
+
+
+def get_property(
+        prop: str,
+        project: str
+) -> str:
+
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+        open(project + '/_version.py').read())
+
+    return result.group(1)
+
+
+project_name = 'reinspy'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=project_name,
+    version=get_property('__version__', project_name),
+    author="Gene Dan",
+    author_email="genedan@gmail.com",
+    description="mixao - Terminal-based music player",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/genedan/mixao",
+    project_urls={
+        "Documentation": "https://reinspy.com/docs"
+    },
+    install_requires=[
+        'textual'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.10.0',
+)
